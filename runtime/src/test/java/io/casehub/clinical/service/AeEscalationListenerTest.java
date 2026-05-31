3acc1e6ccd6f5aadcfb128ef82173a8ecbaf18bc
Mark Proctor <mdproctor@gmail.com>
2026-05-31 01:56:20 +0100

    chore(issue-48): remove redundant static imports in AeEscalationListenerTest
    Refs #48

--- runtime/src/test/java/io/casehub/clinical/service/AeEscalationListenerTest.java
+++ runtime/src/test/java/io/casehub/clinical/service/AeEscalationListenerTest.java
@@ -25,8 +25,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.never;
 
 @ExtendWith(MockitoExtension.class)
 class AeEscalationListenerTest {
